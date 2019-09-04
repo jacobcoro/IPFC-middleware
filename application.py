beb647c442c2d91098ea20cc94a71705da938bad
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2019-09-04 16:51:27 +0800

    get salt trial

--- application.py
+++ application.py
@@ -1,16 +1,36 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+import psycopg2
 
 app = Flask(__name__)
 api = Api(app)
 
+IPFCdatabase_login = """
+dbname='IPFCdatabase' 
+user='jacob' 
+password='5w42bjnscjny8ufy'
+host='165.22.144.86'
+port='5432'
+"""
 
-class HelloWorld(Resource):
+
+class GetSalt(Resource):
     def get(self):
-        return {'hello': 'world'}
+        try:
+            conn = psycopg2.connect(IPFCdatabase_login)
+            cursor = conn.cursor()
+        except:
+            result = "Unable to connect to the database"
+            return result
+        else:
+            email = request.form['email']
+            salt_query = "SELECT salt FROM admin.users WHERE email = %s"
+            cursor.execute(salt_query, (email,))
+            stored_salt = cursor.fetchone()[0]
+            return stored_salt
 
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(GetSalt, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
