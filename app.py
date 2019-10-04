f6287c1499868af45db2f5f7157a8b5e8d225d50
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2019-10-04 18:20:40 +0800

    json dump?123

--- app.py
+++ app.py
@@ -156,15 +156,15 @@ class UserCollection(Resource):
             return result
         else:
             user_id = request.form['user_id']
-            deck_ids = request.form['deck_ids']
+            deck_ids = json.dumps(request.form['deck_ids'])
             cursor.execute('''UPDATE public.user_collections
             SET deck_ids = %s
             WHERE user_id = %s
-            VALUES(%s, %s)''', (user_id, json.dumps(deck_ids)))
+            VALUES(%s, %s)''', (user_id, deck_ids))
             conn.commit()
             cursor.close()
             result = "success"
-            return result
+            return deck_ids
 
 class GetDeck(Resource):
     def get(self):
