ed344e4e3faefd6a6e636171dd3d49de6176cc47
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2019-11-22 23:57:14 +0800

    added cors

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-from flask_cors import CORS
+from flask_restful.utils import cors
+# from flask_cors import CORS
 import psycopg2
 import os
 import json
 import uwsgi
 
 app = Flask(__name__)
-CORS(app)
 api = Api(app)
+app.decorators=[cors.crossdomain(origin='*')]
 DATABASE_URL = os.environ['DATABASE_URL']
 
 class GetSalt(Resource):
