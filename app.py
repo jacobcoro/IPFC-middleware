53f29992b6bbe33f5449c5873d9eb25159fbc93a
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2019-12-01 01:12:42 +0800

    decks get

--- app.py
+++ app.py
@@ -237,8 +237,11 @@ def put_deck(current_user):
         deck_update.deck = data['deck']
     if 'title' in data:
         deck_update.title = data['title']
+        # might be redundant
+        deck_update.deck['title'] = data['title']
     if 'edited' in data:
         deck_update.edited = data['edited']
+        deck_update.deck['edited'] = data['edited']
     if 'deck_cid' in data:
         deck_update.deck_cid = data['deck_cid']
 
