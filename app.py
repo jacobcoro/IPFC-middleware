273dce3676ede575e6268d781cc7a18cd49d9d2e
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-01-14 00:38:30 +0800

    pinata

--- app.py
+++ app.py
@@ -307,10 +307,7 @@ def get_decks(current_user):
 @token_required
 def put_deck(current_user):
     data = request.get_json()
-    print(json.dumps(data, indent=1))
-    sys.stdout.flush()
-
-    deck_id = data['deck']['deck_id']
+    deck_id = data['deck_id']
     deck_update = Decks.query.filter_by(deck_id=deck_id).first()
     pinata_api = current_user.pinata_api
     pinata_key = current_user.pinata_key
